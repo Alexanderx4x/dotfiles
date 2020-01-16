96832126d707faac55f67c0b6d908057a34a8260
Alexander <alexander.x4x@outlook.com>
2020-01-16 17:34:29 +0100

    New commit

--- setup.sh
+++ setup.sh
@@ -1,2 +1,15 @@
-echo ./zsh/.zshrc > ~/.zshrc
-echo ./dotfiles/zsh/.zprofile > ~/.zprofile 
+################################################ SETUP - BASH
+echo "source ~/dotfiles/bash/.bashrc" > ~/.bashrc
+echo "source ~/dotfiels/bash/.bash_profile" > ~/.bash_profile
+
+
+################################################ SETUP - ZSH
+echo "source ~/dotfiles/zsh/.zshrc" > ~/.zshrc
+echo "source ~/dotfiles/zsh/.zprofile" > ~/.zprofile
+
+
+################################################ SETUP - TMUX
+echo "source ~/dotfiles/tmux/.tmux.conf" > ~/.tmux.conf
+
+
+################################################ SETUP - VIM
